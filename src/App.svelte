f4c0796fd088bd053ca70a134410590413dd9b47
neutralboy <leopoorna99@gmail.com>
2025-01-20 15:01:44 +0530

    replace form structure with a simple hello world header

--- src/App.svelte
+++ src/App.svelte
@@ -1,36 +1,10 @@
 <svelte:options customElement="demo-form" />
 
 <script lang="ts">
-  import "medblocks-ui";
-  import "medblocks-ui/dist/styles";
 </script>
 
 <main>
-  <mb-context path="mini-encounter-test-1/context/start_time"></mb-context>
-  <mb-context path="mini-encounter-test-1/context/setting"></mb-context>
-  <div
-    style="display: flex; column-gap: 0.5rem;"
-  >
-    <p
-      style="margin-top: auto; margin-bottom: auto; ">
-      Diagnosis
-    </p>
-    <mb-input
-      id="text_field"
-      path="mini-encounter-test-1/problem_diagnosis/problem_diagnosis_name"
-    ></mb-input>
-  </div>
-
-  <mb-context path="mini-encounter-test-1/problem_diagnosis/language"
-  ></mb-context>
-  <mb-context path="mini-encounter-test-1/problem_diagnosis/encoding"
-  ></mb-context>
-  <mb-context path="mini-encounter-test-1/problem_diagnosis/subject"
-  ></mb-context>
-  <mb-context path="mini-encounter-test-1/category"></mb-context>
-  <mb-context path="mini-encounter-test-1/language"></mb-context>
-  <mb-context path="mini-encounter-test-1/territory"></mb-context>
-  <mb-context path="mini-encounter-test-1/composer"></mb-context>
+  <h1>Hello world</h1>
 </main>
 
 <style>
